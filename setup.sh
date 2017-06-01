203995d4bf0d74c7d7b94821843033d071daca57
glennswest <glennswest@neuralcloudcomputing.com>
2017-06-01 04:41:33 +0000

    Initial Working ansible deploying arm

---
+++ setup.sh
@@ -0,0 +1,13 @@
+yum install python2-pip
+yum install ansible
+yum install python-dev
+yum search python | grep dev
+yum install python-devel
+yum search python | grep dev
+yum search openssh | grep dev
+yum search openssl | grep dev
+yum install openssl-devel
+pip install packaging
+pip install "azure==2.0.0rc5"
+pip install msrestazure
+cat ~/.ssh/id_rsa | base64 > ~/.ocpazure/sshPrivateData
